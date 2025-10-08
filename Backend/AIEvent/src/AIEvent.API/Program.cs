3d18e6fa4943073488fbc39ffed9f30dec5310f1
KietBap <kietanh2622003@gmail.com>
2025-10-08 23:55:39 +0700

    Test https

--- Backend/AIEvent/src/AIEvent.API/Program.cs
+++ Backend/AIEvent/src/AIEvent.API/Program.cs
@@ -1,12 +1,14 @@
-using AIEvent.API.Extensions;
+﻿using AIEvent.API.Extensions;
 using AIEvent.API.Middleware;
 using AIEvent.Application.Constants;
 using AIEvent.Application.DTOs.Common;
+using AIEvent.Domain.Enums;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.FileProviders;
 using StackExchange.Redis;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using static System.Net.WebRequestMethods;
 
 namespace AIEvent.API
 {
@@ -15,6 +17,20 @@ namespace AIEvent.API
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            // Chỉ cấu hình HTTPS khi chạy Production
+            if (builder.Environment.IsProduction())
+            {
+                builder.WebHost.ConfigureKestrel(options =>
+                {
+                    options.ListenAnyIP(80); // HTTP để redirect hoặc ACME
+                    options.ListenAnyIP(443, listenOptions =>
+                    {
+                        listenOptions.UseHttps("/https/aievent.duckdns.org.pfx", builder.Configuration["PFX_PASSWORD"]);
+                    });
+                });
+            }
+
 
             // Add services to the container.
             builder.Services.AddControllers()
@@ -64,12 +80,12 @@ namespace AIEvent.API
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
+            //Configure the HTTP request pipeline.
             //if (app.Environment.IsDevelopment())
             //{
-                
+
             //}
-            
+
             app.UseSwagger();
             app.UseSwaggerUI();
 
