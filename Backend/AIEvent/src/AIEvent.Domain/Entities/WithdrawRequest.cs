b2dd503dca563b0f6693452c3c576e3aa9e46414
Kennenw <nguyenhuuphuc12323@gmail.com>
2025-10-26 14:21:04 +0700

    update delete

--- Backend/AIEvent/src/AIEvent.Domain/Entities/WithdrawRequest.cs
+++ Backend/AIEvent/src/AIEvent.Domain/Entities/WithdrawRequest.cs
@@ -11,8 +11,6 @@ namespace AIEvent.Domain.Entities
         public string BankAccountNumber { get; set; } = "";
         public string BankAccountName { get; set; } = "";
         public WithdrawStatus Status { get; set; } = WithdrawStatus.Pending;
-        public DateTime RequestedAt { get; set; } = DateTime.UtcNow;
-        public DateTime? ProcessedAt { get; set; }
         public User User { get; set; } = default!;
         public string? Note { get; set; }
     }
