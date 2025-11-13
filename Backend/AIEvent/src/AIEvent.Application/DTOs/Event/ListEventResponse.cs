0f2bfcb4599e67710dc96917a7a0a4dcbac9b72e
KietBap <kietanh2622003@gmail.com>
2025-11-13 19:04:32 +0700

    add api get event for staff, update checkin, get report

---
+++ Backend/AIEvent/src/AIEvent.Application/DTOs/Event/ListEventResponse.cs
@@ -0,0 +1,19 @@
+﻿using AIEvent.Application.DTOs.Tag;
+
+namespace AIEvent.Application.DTOs.Event
+{
+    public class ListEventResponse
+    {
+        public required Guid EventId { get; set; }
+        public required string Title { get; set; }
+        public string? Description { get; set; }
+        public required DateTime StartTime { get; set; }
+        public required DateTime EndTime { get; set; }
+        public string? LocationName { get; set; }
+        public int TotalTickets { get; set; }
+        public int SoldQuantity { get; set; }
+        public List<TagResponse>? Tags { get; set; }
+        public required string EventCategoryName { get; set; }
+        public List<string>? ImgListEvent { get; set; }
+    }
+}
