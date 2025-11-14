f43b7a5c4c869f1bd7b8bfe86ca34b1a748c474d
doan15 <146705572+doan15@users.noreply.github.com>
2025-11-14 15:33:53 +0700

    share social

--- Frontend/Mobile/src/constants/Images.js
+++ Frontend/Mobile/src/constants/Images.js
@@ -24,18 +24,25 @@ const Images = {
   // Event images (using existing card images)
   event1: require('../assets/images/card1.png'),
   event2: require('../assets/images/card1.png'), // Using card1 as placeholder
-  
+
   // Background
   background: require('../assets/images/background.png'),
   header: require('../assets/images/background.png'), // Using background as placeholder
-  
+
   // Avatars
   avatar1: require('../assets/images/avatar1.jpg'),
   avatar2: require('../assets/images/avatar1.jpg'), // Using avatar1 as placeholder
-  
+
   // Logo
   aiEventLogo: require('../assets/images/AIEventLogo.png'),
-  
+
+  //share
+  shareSystem: require('../assets/images/system.png'),
+  copy: require('../assets/images/copy.png'),
+  zalo: require('../assets/images/Zalo.png'),
+  facebook: require('../assets/images/Facebook.png'),
+  twitter: require('../assets/images/Twitter.png'),
+  linkedin: require('../assets/images/LinkedIn.png'),
 };
 
 export default Images;
