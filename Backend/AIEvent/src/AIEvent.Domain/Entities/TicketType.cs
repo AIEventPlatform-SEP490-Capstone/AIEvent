48657f56d30e881b5594b6bab6524b9eea42f321
Kennenw <nguyenhuuphuc12323@gmail.com>
2025-11-26 20:23:17 +0700

    update-event

--- Backend/AIEvent/src/AIEvent.Domain/Entities/TicketType.cs
+++ Backend/AIEvent/src/AIEvent.Domain/Entities/TicketType.cs
@@ -15,7 +15,7 @@
         public ICollection<Ticket> Tickets { get; set; } = new List<Ticket>();
         public string? CreatedBy { get; set; }
         public string? UpdatedBy { get; set; }
-        public DateTimeOffset CreatedAt { get; set; }
+        public DateTimeOffset? CreatedAt { get; set; }
         public DateTimeOffset? UpdatedAt { get; set; }
 
         public void SetCreated(string? userId = null)
