1ce35d22e6162bfb4639bd705e8c2ee7c3ec68dc
Kennenw <nguyenhuuphuc12323@gmail.com>
2025-12-20 22:48:56 +0700

    update

--- Backend/AIEvent/src/AIEvent.Application/Services/Implements/DashboardService.cs
+++ Backend/AIEvent/src/AIEvent.Application/Services/Implements/DashboardService.cs
@@ -588,7 +588,7 @@ namespace AIEvent.Application.Services.Implements
                 var currentMonthEvents = await _unitOfWork.EventRepository
                     .Query()
                     .AsNoTracking()
-                    .Where(e => !e.IsDeleted && e.Publish == true && e.Status == EventStatus.Approved &&
+                    .Where(e => !e.IsDeleted && e.Publish == true && e.Status != EventStatus.PendingApproval && e.Status != EventStatus.Rejected &&
                                 e.RequireApprovalAt.HasValue && e.RequireApprovalAt.Value >= currentMonthStart)
                     .CountAsync();
 
